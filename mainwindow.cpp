085639be6180feb13affbe2f9dbf2040ba3c2131
MichalKs <mxiezopolski@gmail.com>
2014-11-01 23:48:21 +0100

    Indents fixed

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,36 +3,39 @@
 #include <QSerialPort>
 #include <QMessageBox>
 #include <QDebug>
+#include <QTextStream>
 #include <iostream>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
 
-    setupUi(this);
+  setupUi(this);
 
-    connect(quitButton, SIGNAL(clicked()), this, SLOT(close()));
+  connect(quitButton, SIGNAL(clicked()), this, SLOT(close()));
 
-    connect(actionExit, SIGNAL(triggered()), this, SLOT(close()));
-    connect(sendFrameButton, SIGNAL(clicked()), this, SLOT(sendData()));
+  connect(actionExit, SIGNAL(triggered()), this, SLOT(close()));
+  connect(sendFrameButton, SIGNAL(clicked()), this, SLOT(sendData()));
 
-    serial = new QSerialPort(this);
-    connect(serial, SIGNAL(readyRead()), this, SLOT(readData()));
-    connect(serial, SIGNAL(error(QSerialPort::SerialPortError)), this,
-            SLOT(handleError(QSerialPort::SerialPortError)));
+  serial = new QSerialPort(this);
+  connect(serial, SIGNAL(readyRead()), this, SLOT(readData()));
+  connect(serial, SIGNAL(error(QSerialPort::SerialPortError)), this,
+          SLOT(handleError(QSerialPort::SerialPortError)));
 
 
-    serial->setPortName("ttyUSB0");
-    serial->setBaudRate(QSerialPort::Baud9600);
-    serial->setDataBits(QSerialPort::Data8);
-    serial->setParity(QSerialPort::NoParity);
-    serial->setStopBits(QSerialPort::OneStop);
-    serial->setFlowControl(QSerialPort::NoFlowControl);
+  serial->setPortName("ttyUSB0");
+  serial->setBaudRate(QSerialPort::Baud9600);
+  serial->setDataBits(QSerialPort::Data8);
+  serial->setParity(QSerialPort::NoParity);
+  serial->setStopBits(QSerialPort::OneStop);
+  serial->setFlowControl(QSerialPort::NoFlowControl);
 
-    if (serial->open(QIODevice::ReadWrite)) {
+  if (serial->open(QIODevice::ReadWrite)) {
 
-    } else {
+  } else {
 
-        QMessageBox::critical(this, tr("Error"), serial->errorString());
-    }
+      QMessageBox::critical(this, tr("Error"), serial->errorString());
+  }
+
+
 
 }
 
@@ -42,27 +45,31 @@ MainWindow::~MainWindow(void) {
 
 void MainWindow::sendData(void) {
 
-    QByteArray a("Hello\n");
-    serial->write(a);
-    serial->waitForBytesWritten(-1);
+  QByteArray a("Hello\n");
+  serial->write(a);
+  serial->waitForBytesWritten(-1);
 
 }
 
 void MainWindow::readData(void) {
-    QByteArray data = serial->readLine();
-    rxData.append(data);
+  QByteArray data = serial->readLine();
+  rxData.append(data);
 
-    if (*(data.data()) == '\n') {
-        std::cout << "Hello: " << rxData.data() << std::endl;
-        rxData.clear();
-    }
+  if (*(data.data()) == '\n') {
+      std::cout << "Hello: " << rxData.data() << std::endl;
+
+//        textEdit->setText();
+      textEdit->append(rxData.data());
+
+      rxData.clear();
+  }
 
 }
 
 void MainWindow::handleError(QSerialPort::SerialPortError error)
 {
-    if (error == QSerialPort::ResourceError) {
-        QMessageBox::critical(this, tr("Critical Error"), serial->errorString());
-        serial->close();
-    }
+  if (error == QSerialPort::ResourceError) {
+      QMessageBox::critical(this, tr("Critical Error"), serial->errorString());
+      serial->close();
+  }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,28 +1,26 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
 #include "ui_mainwindow.h"
+#include <QMainWindow>
 #include <QSerialPort>
 
+class MainWindow : public QMainWindow, public Ui::MainWindow {
 
-
-class MainWindow : public QMainWindow, public Ui::MainWindow
-{
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow(void);
+  explicit MainWindow(QWidget *parent = 0);
+  ~MainWindow(void);
 
 private slots:
-    void sendData(void);
-    void handleError(QSerialPort::SerialPortError error);
-    void readData(void);
+  void sendData(void);
+  void handleError(QSerialPort::SerialPortError error);
+  void readData(void);
 
 private:
-    QSerialPort* serial;
-    QByteArray rxData;
+  QSerialPort* serial;
+  QByteArray rxData;
 };
 
 #endif // MAINWINDOW_H
